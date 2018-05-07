acdbc79b7d170e1661fb6f1c2422c105eb06878f
Derek Damron <damrondm@gmail.com>
2018-05-06 17:38:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='data_structures',
       version='0.1',
       description='Implementation of various data structures',
-      url='http://github.com/ddamv/tictactoe',
+      url='http://github.com/ddamv/data_structures',
       author='Derek Damron',
       author_email='damrondm@gmail.com',
       license='MIT',
