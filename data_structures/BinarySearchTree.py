34635d185a48dd17e2856695874338c75a11646e
Derek Damron <damrondm@gmail.com>
2018-06-02 14:55:01 -0700

    Add insert, as_list, print_as_tree, and get_height

--- data_structures/BinarySearchTree.py
+++ data_structures/BinarySearchTree.py
@@ -9,3 +9,75 @@ class BinarySearchTree(object):
     def __init__(self):
         self._root = None
         return
+        
+    def insert(self, value):
+        new_node = NodeBinarySearchTree(value)
+        if self._root is None:
+            self._root = new_node
+        else:
+            self._insert(new_node, self._root)
+        return
+            
+    def _insert(self, new_node, current_node):
+        if new_node._value == current_node._value:
+            raise ValueError("value already exists")
+        elif new_node._value < current_node._value:
+            if current_node._left is None:
+                current_node._left = new_node
+            else:
+                self._insert(new_node, current_node._left)
+        else:
+            if current_node._right is None:
+                current_node._right = new_node
+            else:
+                self._insert(new_node, current_node._right)
+        return
+        
+    def as_list(self):
+        if self._root is None:
+            return []
+        return self._as_list(self._root, [])
+            
+    def _as_list(self, current_node, current_list):
+        # Return left-hand stuff if exists
+        if current_node._left is not None:
+            self._as_list(current_node._left, current_list)
+        # Return current
+        current_list += [current_node._value]
+        # Return right-hand stuff if exists
+        if current_node._right is not None:
+            self._as_list(current_node._right, current_list)
+        return current_list
+        
+    def print_as_tree(self):
+        if self._root is None:
+            return ""
+        self._print_as_tree(self._root, 0)
+        return
+        
+    def _print_as_tree(self, current_node, left_buffer):
+        if current_node._right is not None:
+            self._print_as_tree(current_node._right, left_buffer + len(str(current_node._value)))
+        print(" " * left_buffer + str(current_node._value))
+        if current_node._left is not None:
+            self._print_as_tree(current_node._left, left_buffer + len(str(current_node._value)))
+        return
+        
+    def get_height(self):
+        if self._root is None:
+            return 0
+        return self._get_height(self._root, 1)
+        
+    def _get_height(self, current_node, current_height):
+        # Traverse left if exists
+        if current_node._left is not None:
+            left_height = self._get_height(current_node._left, current_height + 1)
+        else:
+            left_height = current_height
+        # Traverse right if exists
+        if current_node._right is not None:
+            right_height = self._get_height(current_node._right, current_height + 1)
+        else:
+            right_height = current_height
+        return max(left_height, right_height)
+        
--- data_structures/tests/test_BinarySearchTree.py
+++ data_structures/tests/test_BinarySearchTree.py
@@ -38,3 +38,92 @@ class Test_create_BinarySearchTree(object):
         
     def test_root(self, b):
         assert b._root is None
+
+#####
+# as_list, print_as_tree, and get_height for empty tree
+#
+
+class Test_list_print_and_height(object):
+    def test_empty_tree(self, capsys):
+        l = BinarySearchTree()
+        assert l.get_height() == 0
+        assert l.as_list() == []
+        l.print_as_tree()
+        out, err = capsys.readouterr()
+        assert out == ''
+        
+#####
+# Insert
+#
+
+class Test_insert(object):
+    def test_insert_root(self, capsys):
+        l = BinarySearchTree()
+        l.insert(1)
+        assert l.get_height() == 1
+        assert l.as_list() == [1]
+        l.print_as_tree()
+        out, err = capsys.readouterr()
+        assert out == '1' + '\n'
+        
+    def test_insert_both_sides(self, capsys):
+        l = BinarySearchTree()
+        l.insert(2)
+        l.insert(1)
+        l.insert(3)
+        assert l.get_height() == 2
+        assert l.as_list() == [1, 2, 3]
+        l.print_as_tree()
+        out, err = capsys.readouterr()
+        assert out == (' 3' + '\n' + 
+                       '2' + '\n' +
+                       ' 1' + '\n')
+        
+    def test_insert_complete_tree(self, capsys):
+        l = BinarySearchTree()
+        l.insert(5)
+        l.insert(3)
+        l.insert(4)
+        l.insert(2)
+        l.insert(8)
+        l.insert(9)
+        l.insert(7)
+        assert l.get_height() == 3
+        assert l.as_list() == [2, 3, 4, 5, 7, 8, 9]
+        l.print_as_tree()
+        out, err = capsys.readouterr()
+        assert out == ('  9' + '\n' + 
+                       ' 8' + '\n' + 
+                       '  7' + '\n' + 
+                       '5' + '\n' +
+                       '  4' + '\n' + 
+                       ' 3' + '\n' + 
+                       '  2' + '\n')
+        
+    def test_insert_imbalanced_tree(self, capsys):
+        l = BinarySearchTree()
+        l.insert(5)
+        l.insert(4)
+        l.insert(3)
+        l.insert(2)
+        l.insert(7)
+        l.insert(8)
+        l.insert(9)
+        assert l.get_height() == 4
+        assert l.as_list() == [2, 3, 4, 5, 7, 8, 9]
+        l.print_as_tree()
+        out, err = capsys.readouterr()
+        assert out == ('   9' + '\n' + 
+                       '  8' + '\n' + 
+                       ' 7' + '\n' + 
+                       '5' + '\n' +
+                       ' 4' + '\n' + 
+                       '  3' + '\n' + 
+                       '   2' + '\n')
+        
+    def test_insert_existing_value(self):
+        with pytest.raises(ValueError) as excinfo:
+            l = BinarySearchTree()
+            l.insert(1)
+            l.insert(1)
+        assert 'value already exists' in str(excinfo.value)
